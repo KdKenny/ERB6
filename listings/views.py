bdbc7a9bacb2a64ebf2fcbabf753fc0636645bec
KTK <kwongtkkenny@gmail.com>
2025-06-03 10:19:56 +0800

    Step 11 : index page search function

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from listings.models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import price_choices, bedroom_choices, district_choices
 
 # Create your views here.
 def index(request):
@@ -17,4 +18,8 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {"listings": search,
+               "price_choices" : price_choices,
+               "bedroom_choices" : bedroom_choices,
+               "district_choices" : district_choices}
+    return render(request, 'listings/search.html', context)
