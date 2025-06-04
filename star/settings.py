47d0442c5b58775dcd18e9ca221eaa56cace540d
KTK <kwongtkkenny@gmail.com>
2025-06-04 15:47:11 +0800

    Step 13 : login & register

--- star/settings.py
+++ star/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 from dotenv import load_dotenv
 import os
@@ -145,3 +146,8 @@ INTERNAL_IPS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+MESSAFGE_TAGS = {
+    messages.ERROR : 'danger',
+    messages.SUCCESS : 'success',
+}
